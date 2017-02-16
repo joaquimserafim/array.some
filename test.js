e22374d50f693b1bd31d300428dc990bba4a07db
Joaquim Serafim <joaquim.serafim@gmail.com>
2017-02-16 12:05:38 +0000

    pre-release

---
+++ test.js
@@ -0,0 +1,31 @@
+/*
+eslint
+no-multi-spaces: ["error", {exceptions: {"VariableDeclarator": true}}]
+padded-blocks: ["error", {"classes": "always"}]
+max-len: ["error", 80]
+*/
+'use strict'
+
+const test = require('tape')
+
+const some = require('.')
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+test('', (assert) => {
+
+  assert
+    .deepEqual(
+      some(arr, ele => { return ele > 5 }),
+      true,
+      'should return true'
+    )
+
+  assert
+    .deepEqual(
+      some(arr, ele => { return ele > 10 }),
+      false, 'should return false'
+    )
+
+  assert.end()
+})
